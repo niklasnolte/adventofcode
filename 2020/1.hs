cd83056b8cf26597b28c9a7a49ffe2372a0a4d6c
nnolte <niklas.nolte@cern.ch>
2020-12-03 12:13:43 +0100

    fix 1 and enable assertions

--- 2020/1.hs
+++ 2020/1.hs
@@ -1,12 +1,14 @@
 import Control.Monad (guard)
 import Control.Exception (assert)
 import Safe (headMay)
+import Data.Maybe (isNothing)
 
 part1 :: [Int] -> Maybe Int
 part1 xs = headMay $ do
   xi <- xs
   xj <- xs
   guard $ xi + xj == 2020
+  guard $ xi /= xj
   return $ xi*xj
 
 test_part1 :: IO()
@@ -14,7 +16,7 @@ test_part1 = do
   let testSucc = [200,1800,1620,400,2010] :: [Int]
   let testFail = [1,2,3,4,5] :: [Int]
   assert (part1 testSucc == Just 648000) $
-    assert (part1 testFail == Nothing) $
+    assert (isNothing $ part1 testFail) $
     return ()
 
 part2 :: [Int] -> Maybe Int
@@ -23,6 +25,7 @@ part2 xs = headMay $ do
   xj <- xs
   xk <- xs
   guard $ xi + xj + xk == 2020
+  guard $ (xi /= xj) && (xi /= xk) && (xj /= xk)
   return $ xi*xj*xk
 
 test_part2 :: IO()
@@ -30,7 +33,7 @@ test_part2 = do
   let testSucc = [200,1800,1620,400,2010,1420] :: [Int]
   let testFail = [1,2,3,4,5] :: [Int]
   assert (part2 testSucc == Just 113600000) $
-    assert (part2 testFail == Nothing) $
+    assert (isNothing $ part2 testFail) $
     return ()
 
 getInput :: IO [Int]

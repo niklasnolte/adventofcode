89a8d0a43b86cca80be7882ab92e3cc1babf4fa0
nnolte <niklas.nolte@cern.ch>
2021-12-06 11:11:30 -0500

    small refactor

--- a2021/6.jl
+++ a2021/6.jl
@@ -1,32 +1,31 @@
 function n_fish(state::Int32)
-  if state >= 0
-    return 1
-  end
-  states = [[1];repeat([0],8)]
-  for i in 1:-state
-    ripefish = states[1]
-    for n in 1:8
-      states[n] = states[n+1]
+    if state >= 0
+        return 1
     end
-    states[7] += ripefish
-    states[9] = ripefish
-  end
-  return sum(values(states))
+    states = [[1]; repeat([0], 8)]
+    for i = 1:-state
+        ripefish = states[1]
+        for n = 1:8
+            states[n] = states[n+1]
+        end
+        states[7] += ripefish
+        states[9] = ripefish
+    end
+    return sum(values(states))
 end
 
-function partX(fish::Vector{Int32}, n_days)
-  fish .-= n_days
-  sum(n_fish.(fish)) |> println
+function partX(fish::Vector{Int32}, n_days::Int32)
+    fishEvolved = fish .- n_days
+    sum(n_fish.(fishEvolved)) |> println
 end
 
-part1(fish) = partX(fish, 80)
-part2(fish) = partX(fish, 256)
+part1(fish) = partX(fish, Int32(80))
+part2(fish) = partX(fish, Int32(256))
 
 function main()
-  states = parse.(Int32, split(readlines("input6.txt")[1], ","))
-  part1(copy(states))
-  part2(copy(states))
+    states = parse.(Int32, split(readlines("input6.txt")[1], ","))
+    part1(states)
+    part2(states)
 end
-
 
 main()
